11164a848df7119c5f7f409af29484f0eb2fe182
IoanaTC <ioanateodoraconstantin@yahoo.com>
2023-11-05 22:11:42 +0200

    parser pentru NT headers - first commit

--- src/compile.bat
+++ src/compile.bat
@@ -4,4 +4,4 @@ g++ -c parser.cpp -o parser
 g++ -c main.cpp -o main
 g++ parser main -o packer
 
-packer.exe ..\..\..\work\setup-lightshot.exe
+packer.exe ..\..\..\..\..\work\setup-lightshot.exe
--- src/parser.h
+++ src/parser.h
@@ -9,18 +9,21 @@ class PEparser
 {
     public:
         IMAGE_DOS_HEADER dosHeader;
-    
+        BYTES* dosStub;
 
         HANDLE inputFile;
 
         PEparser(HANDLE inputFile);
         bool parsePE();
 
+
         ~PEparser();
 
     private:
 
         bool readDosHeader();
+        bool readDosStub();
+        bool readNTheaders();
 
 };
 
